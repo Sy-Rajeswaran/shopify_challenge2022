a5a5fb1f5ad13fbf79b057f1ddf95249de7effa5
Sayanan Rajeswaran <sayananrajeswaran@Sayanans-MacBook-Air.local>
2021-09-13 21:22:04 -0400

    changed the layout to nicer grid layout with seperate boxes

--- src/components/imageComp.js
+++ src/components/imageComp.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import style from './imageComp.css'
+import style from './imageComp.module.css'
 const Images= ({image,earthdate,rovername,cameraname})=>{
     return (
         <div className={style.images}>
-            <img src={image} alt="Mars Rover" className={style.image}></img>
-            <p>The {rovername} rover with its {cameraname}</p>
-
+            <img src={image} alt="Mars Rover" className={style.pics}></img>
+            <p className={style.caption}>The {rovername} rover with its {cameraname}</p>
+            <p className={style.date}>{earthdate}</p>
         </div>
 
     );
